70837b29031d9495adfbba813c71a5bcb88ce701
TonO <tonot1@hotmail.com>
2024-11-21 12:05:38 +0100

    Add config file of all modules writing via metaMessage.js

---
+++ meta/logComponents.js
@@ -0,0 +1,3 @@
+const logmodules = {'MetaComponents':["meta","directoryHelper","imageHelper","labelHelper","metaController","metaMessage","processingManager","sensorHelper","sliderHelper","switchHelper","variablesVault","telnet-client"]}
+module.exports = logmodules;
+
