2bca3aedc7b7ae36da7c273ca7445d9223d42e26
TonO <tonot1@hotmail.com>
2024-12-08 15:19:17 +0100

    Update logComponents.js

--- meta/logComponents.js
+++ meta/logComponents.js
@@ -1,3 +1,3 @@
-const logmodules = {'MetaComponents':["meta","directoryHelper","imageHelper","labelHelper","metaController","metaMessage","processingManager","sensorHelper","sliderHelper","switchHelper","variablesVault","telnet-client"]}
+const logmodules = {'MetaComponents':["meta","directoryHelper","imageHelper","labelHelper","metaController","metaMessage","processingManager","sensorHelper","sliderHelper","switchHelper","variablesVault","telnet-client","CP6","deviceadapter","directoryadapter","imageserver","homekit"]}
 module.exports = logmodules;
 
