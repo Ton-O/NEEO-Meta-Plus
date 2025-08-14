0fbf030edcd2e91007ec489610dd30214152276f
TonO <tonot1@hotmail.com>
2025-08-14 10:13:38 +0200

    Fix typo in logComponents: as convention, the filename of the components are used.. Now using Capital P for processingmanager

--- logComponents.js
+++ logComponents.js
@@ -1,3 +1,3 @@
-const logModules = {'MetaComponents':["meta","directoryHelper","imageHelper","labelHelper","metaController","metaMessage","processingManager","sensorHelper","sliderHelper","switchHelper","variablesVault","telnet-client"]}
+const logModules = {'MetaComponents':["meta","directoryHelper","imageHelper","labelHelper","metaController","metaMessage","ProcessingManager","sensorHelper","sliderHelper","switchHelper","variablesVault","telnet-client"]}
 const  produceNrSnapshotWhenError = 200;
 module.exports = {logModules,produceNrSnapshotWhenError};
