43ab08771168e204516bb0436ee8c719c73080e0
TonO <tonot1@hotmail.com>
2026-05-13 11:28:24 +0200

    Typos

--- Documentation/make-a-metavlan.sh
+++ Documentation/make-a-metavlan.sh
@@ -4,4 +4,4 @@ docker network create -d macvlan    --subnet=192.168.73.0/24 --gateway=192.168.7
 # You only need to create this metavlan once... 
 # subnet: your host network (like 192.168.73.0) with it's subnet: /24
 # gateway: your normal gateway in your network (most of the time your router)
-# parent=eno1: the network interface of your host which is connected to the network (you can find it with ifconfig or ip addr)
+# parent=eno1: the network interface of your host which is connected to the network (you can find it with ifconfig or ip addr on your host)
